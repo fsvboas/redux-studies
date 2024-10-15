51a310920239f307d75bff558427580e5a0b3759
fsvboas <fsvboas.dev@gmail.com>
2024-10-15 15:47:35 -0300

    feat: create header and footer components

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-14 w-full flex items-center justify-center">
+      <h1 className="text-white text-2xl">Redux Studies - Virtual Assistant</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Container from "./components/core/container";
+import Footer from "./components/footer";
+import Header from "./components/header";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 pb-20 sm:p-20 bg-black/95">
-      <h1 className="text-2xl text-white">Hello World</h1>
+    <div className="flex justify-center min-h-screen bg-black/95 py-10">
+      <Container>
+        <Header />
+        <main className="h-full w-full bg-black/20 my-8 p-4 rounded-lg">
+          <div>
+            <p className="font-bold text-white">Eu:</p>
+            <p className="text-white">Olá!</p>
+          </div>
+        </main>
+        <Footer />
+      </Container>
     </div>
   );
 }
